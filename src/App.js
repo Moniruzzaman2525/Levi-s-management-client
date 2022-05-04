50254b6407ac47153c08ca46d57b1f2c2d429488
unknown <md616264@gmail.com>
2022-05-04 17:36:56 +0600

    log in page start

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Pages/Home/Home';
 import Update from './Pages/Update/Update';
 import ManageItems from './Pages/ManageItmes/ManageItems';
 import AddItem from './Pages/ManageItmes/AddItem/AddItem';
+import Signin from './Pages/SigninPage/Signin/Signin';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/manage' element={<ManageItems></ManageItems>}></Route>
         <Route path='/add' element={<AddItem></AddItem>}></Route>
         <Route path='/update/:id' element={<Update></Update>}></Route>
+        <Route path='signin' element={<Signin></Signin>}></Route>
       </Routes>
 
     </div>
