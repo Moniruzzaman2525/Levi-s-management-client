db0eb4a4f48f63290f789718503b795f271086ad
unknown <md616264@gmail.com>
2022-05-04 07:30:18 +0600

    navbar add

---
+++ src/Pages/Banner/Banner.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const Banner = () => {
+    return (
+        <>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="img1"></div>
+                </SwiperSlide>
+                {/* <SwiperSlide>
+                    <img src="https://i.ibb.co/NjDF3Lc/1.png" alt="" />
+                </SwiperSlide> */}
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+        </>
+    );
+};
+
+export default Banner;
