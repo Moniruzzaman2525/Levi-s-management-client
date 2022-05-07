f973e410e2b986a9594ca55a51bf4eec69eca589
unknown <md616264@gmail.com>
2022-05-07 11:49:35 +0600

    sign in page design

--- src/Pages/SigninPage/Signin/Signin.js
+++ src/Pages/SigninPage/Signin/Signin.js
@@ -4,6 +4,9 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
+import { IoLogoGoogleplus } from 'react-icons/io';
+import { FaGithub } from 'react-icons/fa';
+import { RiFacebookCircleFill } from 'react-icons/ri';
 
 const Signin = () => {
     const navigate = useNavigate()
@@ -80,22 +83,39 @@ const Signin = () => {
 
 
     return (
-        <div className='w-2/4 mx-auto'>
-            <form onSubmit={handleSignIn}>
-                <div className="mb-6">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                    <input type="email" onChange={handleEmailChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    {errors?.email && <p className='text-red-600 font-bold mt-2'>{errors.email}</p>}
+        <div className='md:grid grid-cols-2'>
+            <div className=''>
+                <h2 className='text-center text-3xl py-10'>Sign in to....</h2>
+                <div className='flex justify-center'>
+                    <RiFacebookCircleFill className='mr-5'></RiFacebookCircleFill>
+                    <IoLogoGoogleplus className='mr-5'></IoLogoGoogleplus>
+                    <FaGithub className='mr-5'></FaGithub>
                 </div>
-                <div className="mb-6">
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-                    <input type="password" onChange={handlePassChange} id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    {errors?.password && <p className='text-red-600 font-bold mt-2'>{errors.password}</p>}
-                </div>
+                <p className='text-center'>or use your email account</p>
+                <div className='w-3/6 mx-auto py-20 h-[50vh]'>
+                    <form onSubmit={handleSignIn}>
+                        <div className="mb-6">
+                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                            <input type="email" onChange={handleEmailChange} id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            {errors?.email && <p className='text-red-600 font-bold mt-2'>{errors.email}</p>}
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
+                            <input type="password" onChange={handlePassChange} id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            {errors?.password && <p className='text-red-600 font-bold mt-2'>{errors.password}</p>}
+                        </div>
 
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                <p>Don't have an account? <Link to="/signup">Signup</Link></p>
-            </form>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+                    </form>
+                </div>
+            </div>
+            <div className="login-img relative">
+                <div className='absolute bottom-0 left-0 right-0 top-[40%] '>
+                    <h1 className='text-center text-4xl mb-10 font-bold'>Hello, Friend</h1>
+                    <h5 className='text-center text-xl'>Enter your personal details <br /> and start journey with us</h5>
+                </div>
+            </div>
         </div>
     );
 };
